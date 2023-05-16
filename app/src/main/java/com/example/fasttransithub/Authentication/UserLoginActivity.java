09aceec82087fd1fb0fb65255c6c12e813b8180f
saadzafar649 <70218806+saadzafar649@users.noreply.github.com>
2023-05-16 14:20:02 +0500

    nameicon added

---
+++ app/src/main/java/com/example/fasttransithub/Authentication/UserLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.fasttransithub.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fasttransithub.R;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
