5582724dd0cb484654415c08ca294dc15af63cc5
saira123iqbal <f200174@cfd.nu.edu.pk>
2023-05-28 23:23:12 +0500

    user fragments

--- app/src/main/java/com/example/fasttransithub/User/DashboardActivity.java
+++ app/src/main/java/com/example/fasttransithub/User/DashboardActivity.java
@@ -1,16 +1,67 @@
 package com.example.fasttransithub.User;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.fasttransithub.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity
+        implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard2);
+
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.User);
+    }
+
+    user_home_Fragment user_home_Fragment = new user_home_Fragment();
+    user_schedule_Fragment user_schedule_Fragment = new user_schedule_Fragment();
+    user_Account_Fragment user_Account_Fragment = new user_Account_Fragment();
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item)
+    {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.home, user_home_Fragment)
+                        .commit();
+                return true;
+
+            case R.id.schedule:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.schedule,user_schedule_Fragment)
+                        .commit();
+                return true;
+
+            case R.id.account:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.account,  user_Account_Fragment)
+                        .commit();
+                return true;
+        }
+        return false;
     }
 }
