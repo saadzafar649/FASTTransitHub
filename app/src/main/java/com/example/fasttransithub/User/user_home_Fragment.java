f3094a4aa0679856b472b4ba138e3a46a8fc6744
saira123iqbal <f200174@cfd.nu.edu.pk>
2023-05-30 17:25:17 +0500

    user home

--- app/src/main/java/com/example/fasttransithub/User/user_home_Fragment.java
+++ app/src/main/java/com/example/fasttransithub/User/user_home_Fragment.java
@@ -1,5 +1,9 @@
 package com.example.fasttransithub.User;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +11,69 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 
 import com.example.fasttransithub.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link user_home_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class user_home_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private ImageView qrCodeIV;
+    Bitmap bitmap;
     public user_home_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment user_home_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static user_home_Fragment newInstance(String param1, String param2) {
-        user_home_Fragment fragment = new user_home_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_home_, container, false);
+       View view= inflater.inflate(R.layout.fragment_user_home_, container, false);
+       qrCodeIV=view.findViewById(R.id.user_Qr);
+        initQRCode();
+//        String myText = "Something";
+//        //initializing MultiFormatWriter for QR code
+//        MultiFormatWriter mWriter = new MultiFormatWriter();
+//        try {
+//            //BitMatrix class to encode entered text and set Width & Height
+//            BitMatrix mMatrix = mWriter.encode(myText, BarcodeFormat.QR_CODE, 400,400);
+//            BarcodeEncoder mEncoder = new BarcodeEncoder();
+//            Bitmap mBitmap = mEncoder.createBitmap(mMatrix);//creating bitmap of code
+//            qrCodeIV.setImageBitmap(mBitmap);//Setting generated QR code to imageView
+//            // to hide the keyboard
+//            //InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            //manager.hideSoftInputFromWindow(etText.getApplicationWindowToken(), 0);
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//        }
+        return view;
+    }
+    private void initQRCode() {
+        String name = "Name : ";
+
+        StringBuilder textToSend = new StringBuilder();
+        textToSend.append(name);
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(textToSend.toString(), BarcodeFormat.QR_CODE, 600, 600);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qrCodeIV.setImageBitmap(bitmap);
+            qrCodeIV.setVisibility(View.VISIBLE);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
